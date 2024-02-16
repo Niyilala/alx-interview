efd92033f694971aeacaa64890f46495be86d517
Adeniyi <adeniyi.oluwajana@gmail.com>
2024-02-16 07:55:27 -0800

    primegame2

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,81 @@
 #!/usr/bin/python3
 """
-Prime Game: Determine the winner of the Prime Game.
+Prime Game Module
 """
 
 
+def isWinner(x, nums):
+    """
+    Determines the winner of each game played.
+
+    Args:
+      x (int): The number of rounds.
+      nums (list): An array of n for each round.
+
+    Returns:
+      str: Name of the player who won the most rounds.
+           If the winner cannot be determined, returns None.
+    """
+    if not nums or x < 1:
+        return None
+    players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
+        temp = count_primes(cluster)
+
+        if temp % 2 == 0:
+            players['Ben'] += 1
+        elif temp % 2 != 0:
+            players['Maria'] += 1
+
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Maria'] < players['Ben']:
+        return 'Ben'
+    else:
+        return None
+
+
 def is_prime(num):
-    """Check if a number is prime."""
+    """
+    Checks if a given number is prime.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
     if num < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
 
-def isWinner(x, nums):
-    """Determine the winner of the Prime Game."""
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+def remove_multiples(num, lst):
+    """
+    Removes multiples of a given number from a list
+    """
+    new_lst = [item for item in lst if item % num != 0]
+    return new_lst
 
 
-# Example usage
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+def count_primes(nums):
+    """
+    Counts the number of primes in a given set
+    """
+    counter = 0
+    target = list(nums)
+    for i in range(1, len(target) + 1):
+        if is_prime(i):
+            counter += 1
+            target = remove_multiples(i, target)
+    return counter
